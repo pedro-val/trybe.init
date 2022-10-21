9fc153dd8972add5f9adb104e47b372c2861f4bb
devpedroval <prmval@gmail.com>
2022-10-21 16:44:16 -0300

    formatando js

--- Fundamentos/JavaScript/dia01/scripts6.js
+++ Fundamentos/JavaScript/dia01/scripts6.js
@@ -1,17 +1,17 @@
-let situacao = 'abc';
+let situacao = "abc";
 let message = null;
 
 switch (situacao) {
-    case 'aprovado':
-        message = ('Parabéns, você está aprovado!');
-        break;
-    case 'lista':
-        message = ('Você foi movido para a lista de espera por vagas!');
-        break;
-    case 'reprovado':
-        message = ('Infelizmente você foi reprovado!');
-        break;
-    default:
-        message = ('ainda não temos sua situação');
+  case "aprovado":
+    message = "Parabéns, você está aprovado!";
+    break;
+  case "lista":
+    message = "Você foi movido para a lista de espera por vagas!";
+    break;
+  case "reprovado":
+    message = "Infelizmente você foi reprovado!";
+    break;
+  default:
+    message = "ainda não temos sua situação";
 }
 console.log(message);
