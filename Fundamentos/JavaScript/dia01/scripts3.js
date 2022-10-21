cefddfa3dfcf1a99f19f472bc1cb3603b5d5b90e
devpedroval <prmval@gmail.com>
2022-10-21 16:00:05 -0300

    para fixar 4

--- Fundamentos/JavaScript/dia01/scripts3.js
+++ Fundamentos/JavaScript/dia01/scripts3.js
@@ -1,7 +1,22 @@
+// let nota = 50
+// if (nota >= 80)
+// console.log('Parabéns, você foi aprovada!')
+// else if (nota < 80 && nota >= 60)
+// console.log('Você está na nossa lista de espera')
+// else if (nota < 60 && nota >= 30)
+// console.log('Em recuperação')
+// else
+// console.log('Você foi reprovada.')
+
 let nota = 50
+let mensagem = null
 if (nota >= 80)
-console.log('Parabéns, você foi aprovada!')
+mensagem = ('Parabéns, você foi aprovada!')
 else if (nota < 80 && nota >= 60)
-console.log('Você está na nossa lista de espera')
+mensagem = ('Você está na nossa lista de espera')
+else if (nota < 60 && nota >= 30)
+mensagem = ('Em recuperação')
 else
-console.log('Você foi reprovada.')
+mensagem = ('Você foi reprovada.')
+
+console.log(mensagem)
