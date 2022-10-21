97df99edbbeb0cc5f8368eeee3a05e0a95316c4e
devpedroval <prmval@gmail.com>
2022-10-21 16:03:56 -0300

    para fixar 5

---
+++ Fundamentos/JavaScript/dia01/scripts5.js
@@ -0,0 +1,5 @@
+let weekDay = 'domingo'
+if (weekDay == 'segunda-feira' || weekDay == 'terça-feira' || weekDay == 'quarta-feira' || weekDay == 'quinta-feira' || weekDay == 'sexta-feira')
+console.log('UHULLL mais um dia na trybe!')
+else
+console.log('fim de semana galera, vamos descansar!')
