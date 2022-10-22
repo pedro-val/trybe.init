8c23064c6c7890e44dc956652ae0aeb48bcb5f97
devpedroval <prmval@gmail.com>
2022-10-22 18:33:27 -0300

    problemas resolvidos

--- Fundamentos/JavaScript/dia02/mega sena.js
+++ Fundamentos/JavaScript/dia02/mega sena.js
@@ -1,5 +1,5 @@
 let sorteados = [11, 43, 23, 76, 12, 1, 2];
-let jogo = [12, 2, 11]
+let jogo = [12, 2, 11, 43, 76, 91] 
 let numerosDeAcertos = []
 
 for (let i = 0; i < sorteados.length; i++){
@@ -8,4 +8,10 @@ for (let i = 0; i < sorteados.length; i++){
         numerosDeAcertos.push(jogo[ij])
     }
 }
-console.log(numerosDeAcertos)
+if (numerosDeAcertos.length == 5) {
+    console.log('Voce fez a quina, com os números:' + numerosDeAcertos)
+} else if (numerosDeAcertos.length == 6) {
+    console.log('Você é o ganhador da mega sena, com os números:' + numerosDeAcertos)
+} else {
+    console.log('Infelizmente não acertou números suficientes, números acertados:' + numerosDeAcertos)
+}
