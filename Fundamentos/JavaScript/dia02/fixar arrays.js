0d5304cf89b042e8d2084a90cd9d476de428ce8b
devpedroval <prmval@gmail.com>
2022-10-22 07:19:56 -0300

    para fixar 2

--- Fundamentos/JavaScript/dia02/fixar arrays.js
+++ Fundamentos/JavaScript/dia02/fixar arrays.js
@@ -1,4 +1,9 @@
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = null
+// menuServices = menu[1]
+// console.log(menuServices);
+
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = null
-menuServices = menu[1]
-console.log(menuServices);
+let indexOfPortfolio = null
+indexOfPortfolio = menu.indexOf('Portfólio')
+console.log(indexOfPortfolio);
