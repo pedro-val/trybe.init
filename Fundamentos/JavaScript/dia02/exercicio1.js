039fdeb9a54eb4684a52502cc4595d69a3c99a65
devpedroval <prmval@gmail.com>
2022-10-22 08:07:35 -0300

    exercicios

---
+++ Fundamentos/JavaScript/dia02/exercicio1.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// console.log(numbers);
+// console.log(numbers.length)
+let somaArray = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9]
+let media = somaArray / 2
+// console.log(somaArray)
+// console.log(media)
+if (media > 20) {
+    console.log('valor maior que 20')
+} else {
+    console.log('valor menos ou igual a 20')
+}
