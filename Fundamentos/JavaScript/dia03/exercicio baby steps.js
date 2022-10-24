6b1e47ab80effbbb3e5408db86a10c26b895f0e0
devpedroval <prmval@gmail.com>
2022-10-24 07:47:15 -0300

    exercícios 1 arrays

--- Fundamentos/JavaScript/dia03/exercicio baby steps.js
+++ Fundamentos/JavaScript/dia03/exercicio baby steps.js
@@ -1,8 +1,16 @@
-let fruits = [3, 4, 10, 1, 12];
-let som = 0;
+// let fruits = [3, 4, 10, 1, 12];
+// let som = 0;
 
-for (let i = 0; i < fruits.length; i += 1) {
-  som += fruits[i];
+// for (let i = 0; i < fruits.length; i += 1) {
+//   som += fruits[i];
 
-}
-console.log(som);
+// }
+// console.log(som);
+
+const n = 9;
+
+let resultado = 100;
+for (let index = 0; index <= n; index += 1) {
+  resultado -= index;
+};
+console.log(resultado);
---
+++ Fundamentos/JavaScript/dia03/teste.js
@@ -0,0 +1,13 @@
+// let word = 'tryber';
+// let wordInverse = [];
+// for (i = 0; i = word.length; i++) {
+//     wordInverse.unshift(word[i])
+// }
+// console.log(wordInverse)
+
+let word = 'tryber';
+let inverse = [];
+for (let index = 0; index < word.length; index++) {
+    inverse.unshift(word[index]);
+}
+console.log(inverse)
