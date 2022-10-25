ca04e8b61ef3832ca4b1a976499c88505612bfc3
devpedroval <prmval@gmail.com>
2022-10-25 11:28:28 -0300

    forinoffparafixar

---
+++ Fundamentos/JavaScript/dia04/forinoff.js
@@ -0,0 +1,17 @@
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+//   for ( k in names) {
+//     console.log('Olá ' + names[k])
+//   }
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+  for (let k in car) {
+    console.log(k + car[k])
+  }
