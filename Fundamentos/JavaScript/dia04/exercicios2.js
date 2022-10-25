75e88b25844e9ca8b618da0d014097c9276f1c58
devpedroval <prmval@gmail.com>
2022-10-25 16:41:37 -0300

    exercicio2 objetos

---
+++ Fundamentos/JavaScript/dia04/exercicios2.js
@@ -0,0 +1,18 @@
+//   console.log('O livro favorito de' + ' ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo + '.')
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+leitor.livrosFavoritos.push({titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+autor: 'JK Rowling',
+editora: 'Rocco',})
+console.log(leitor.livrosFavoritos[1])
+console.log('Júlia tem ' + leitor.livrosFavoritos.length + ' livros favoritos.')
