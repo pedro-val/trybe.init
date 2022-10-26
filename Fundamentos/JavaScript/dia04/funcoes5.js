310dfa2e3e6fb0923d8c331e725ab06d3515733d
devpedroval <prmval@gmail.com>
2022-10-26 10:28:56 -0300

    maior posição do array

--- Fundamentos/JavaScript/dia04/funcoes5.js
+++ Fundamentos/JavaScript/dia04/funcoes5.js
@@ -1,11 +1,14 @@
+let maiorPosicao;
 function biggerArrayNumber(array) {
-    let maior = array[0]
+    let maior = array[0];
+    maiorPosicao = 0 ;
     for (let key in array) {
         if (array[key] >= maior) {
             maior = array[key]
+            maiorPosicao = array.indexOf(maior)
         }
     }
-    return console.log(maior)
+    return console.log(maiorPosicao)
 }
 biggerArrayNumber([2, 4, 6, 7, 10, 0, -3])
     
--- Fundamentos/JavaScript/dia04/test.js
+++ Fundamentos/JavaScript/dia04/test.js
@@ -1,6 +1,14 @@
-function biggestName(array) {
-    for (let index = 0; index < array.length; index++) {
-        console.log(array[index]);        
-    }
-}
-biggestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']);
+// function biggerArrayNumber(array) {
+//     let maior = array[0];
+//     let maiorPosicao = array[0].length
+//     maiorPosicao = array[0].length
+//     for (let key in array) {
+//         if (array[key] >= maior) {
+//             maior = array[key]
+//             maiorPosicao = array[key].length
+//         }
+//     }
+//     return console.log(maiorPosicao)
+// }
+let array = [2, 4, 6, 7, 10, 0, -3]
+console.log(array.indexOf(4))
