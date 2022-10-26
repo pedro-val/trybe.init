27afec7c4a4fb329c04cdcaef413fa6e0bcf7c35
devpedroval <prmval@gmail.com>
2022-10-26 19:30:30 -0300

    lint2

--- Fundamentos/JavaScript/dia05/parte2-3.js
+++ Fundamentos/JavaScript/dia05/parte2-3.js
@@ -1,14 +1,14 @@
-longestWord = (frase) => {
-    let separacao = frase.split(' ')
-    let maior = '0'
-    for (let i of separacao) {
-       if (i.length > maior.length) {
-        maior = i
+const longestWord = (frase) => {
+    let separacao = frase.split(' ');
+    let maior = separacao[0];
+    for (let i = 1; i < separacao.length; i += 1) {
+       if (separacao[i].length > maior.length) {
+        maior = separacao[i];
        }
     }
-    return console.log(`A maior palavra da frase é: ${maior}.`)
+    return console.log(`A maior palavra da frase é: ${maior}.`);
 }
-longestWord('Antônio foi noasdasdasdasdasdasd banheiro e não sabemos o que aconteceu')
+longestWord('Antônio foi ao banheiro e não sabemos o que aconteceu');
 // Bônus (opcional): Tente fazer o mesmo exercício utilizando o método array.sort().
 
 // longestWord = (frase) => {
--- Fundamentos/JavaScript/dia05/teste.js
+++ Fundamentos/JavaScript/dia05/teste.js
@@ -1,7 +1,1 @@
-const arr = [1,2,3,4,5,'foo','bar','baz']
-
-console.log(JSON.stringify(arr));
-console.log(JSON.stringify(arr, null, 2));
-console.log(arr.toString());
-console.log(`${arr}`);
-console.log(arr.join('\n'));
+console.log(Math.PI)
