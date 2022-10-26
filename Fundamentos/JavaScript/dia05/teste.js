91f402d7fc169aed16c88e765f90dd0ca1cebb8c
devpedroval <prmval@gmail.com>
2022-10-26 18:21:46 -0300

    bonus feito

--- Fundamentos/JavaScript/dia05/teste.js
+++ Fundamentos/JavaScript/dia05/teste.js
@@ -1,6 +1,8 @@
 const frase = 'Antônio foi no banheiro e não sabemos o que aconteceu'
 let separado = frase.split(' ')
-for (let i of separado){
-    let letras = i.split('').length
-    console.log(letras)
+let maior = '0'
+for (let i in separado){
+    let letras = separado[i].split('').length
+    if (separadomaior.length)
+    
 }
