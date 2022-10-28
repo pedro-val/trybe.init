78d9f52430443fa0bbc3aa12f50946faa8277019
devpedroval <prmval@gmail.com>
2022-10-28 16:13:58 -0300

    Administrador do Tempo da Trybe

---
+++ Fundamentos/JS-Dom-Eventos-Web Storage/dia01/script.js
@@ -0,0 +1,18 @@
+let emergencyH3 = document.querySelectorAll('.emergency-tasks h3');
+let noEmergencyH3 = document.querySelectorAll('.no-emergency-tasks h3');
+document.getElementById('header-container').style.backgroundColor = 'green';
+document.getElementById('footer-container').style.backgroundColor = 'green';
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow';
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'salmon';
+document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = 'purple';
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = 'purple';
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = 'black';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = 'black';
+
+// for (let i in emergencyH3) {
+//     emergencyH3[i].style.backgroundColor = 'purple';
+// }
+// for (let i2 in noEmergencyH3) {
+//     noEmergencyH3[i2].style.backgroundColor = 'black';
+// }
+
