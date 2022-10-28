de8c6481d820b8c2c61c2fb2d42c43a230eb7923
devpedroval <prmval@gmail.com>
2022-10-28 16:16:27 -0300

    duvidas e mais duvidas

--- Fundamentos/JS-Dom-Eventos-Web Storage/dia01/script.js
+++ Fundamentos/JS-Dom-Eventos-Web Storage/dia01/script.js
@@ -1,5 +1,3 @@
-let emergencyH3 = document.querySelectorAll('.emergency-tasks h3');
-let noEmergencyH3 = document.querySelectorAll('.no-emergency-tasks h3');
 document.getElementById('header-container').style.backgroundColor = 'green';
 document.getElementById('footer-container').style.backgroundColor = 'green';
 document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow';
@@ -9,6 +7,8 @@ document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = 'pur
 document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = 'black';
 document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = 'black';
 
+// let emergencyH3 = document.querySelectorAll('.emergency-tasks h3');
+// let noEmergencyH3 = document.querySelectorAll('.no-emergency-tasks h3');
 // for (let i in emergencyH3) {
 //     emergencyH3[i].style.backgroundColor = 'purple';
 // }
