dd2bfb48e69f92d7740fd695135cb95b4c1fb209
devpedroval <prmval@gmail.com>
2022-11-03 16:55:33 -0300

    começando o script

---
+++ Fundamentos/JS-Dom-Eventos-Web Storage/dia04/script.js
@@ -0,0 +1,10 @@
+let corFundo = document.getElementById('corFundo');
+let corFonte = document.getElementById('corFonte');
+let tamanho = document.getElementById('tamanhoFonte');
+let espacamento = document.getElementById('espacamento');
+let fonte = document.getElementById('fonte');
+let main = document.getElementById('main');
+
+corFundo.addEventListener('click', () => {
+
+})
