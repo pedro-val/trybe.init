8ff86709c371f8649a7060ee6c8ecaee22661fe4
devpedroval <prmval@gmail.com>
2022-11-03 17:24:11 -0300

    consegui fazer sozinho oporrrrrr

--- Fundamentos/JS-Dom-Eventos-Web Storage/dia04/script.js
+++ Fundamentos/JS-Dom-Eventos-Web Storage/dia04/script.js
@@ -5,22 +5,36 @@ let espacamento = document.getElementById('espacamento');
 let fonte = document.getElementById('fonte');
 let main = document.getElementById('main');
 
+
+
 corFundo.addEventListener('click', (event) => {
    main.style.backgroundColor = event.target.innerText;
+   localStorage.setItem('corDeFundo', event.target.innerText);
 })
 
 corFonte.addEventListener('click', (event) => {
     main.style.color = event.target.innerText;
+    localStorage.setItem('corFonte', event.target.innerText);
+    
 })
 
 tamanho.addEventListener('click', (event) => {
     main.style.fontSize = event.target.innerText;
+    localStorage.setItem('fontSize', event.target.innerText);
 })
 
 espacamento.addEventListener('click', (event) => {
     main.style.lineHeight = event.target.innerText;
+    localStorage.setItem('lineHeight', event.target.innerText);
 })
 
 fonte.addEventListener('click', (event) => {
     main.style.fontFamily = event.target.innerText;
+    localStorage.setItem('fontFamily', event.target.innerText);
 })
+
+window.onload(main.style.backgroundColor = localStorage.getItem('corDeFundo'),
+main.style.color = localStorage.getItem('corFonte'),
+main.style.fontSize = localStorage.getItem('fontSize'),
+main.style.lineHeight = localStorage.getItem('lineHeight'),
+main.style.fontFamily = localStorage.getItem('fontFamily'))
