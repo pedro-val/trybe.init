8d5581cab4b00a0496dd013047f54227b34d2725
devpedroval <prmval@gmail.com>
2022-11-21 15:14:48 -0300

    comecando exercicios do dia com jest

--- Fundamentos/JS-ES6-Testes unitarios/dia01/script.test.js
+++ Fundamentos/JS-ES6-Testes unitarios/dia01/script.test.js
@@ -1,12 +1,12 @@
 const getValueByNumber = require('./exercicio3');
 
-test('dado um objeto, retorna o valor da chave', () => {
+test('dado um objeto e um numero de chave, retorna o valor da chave', () => {
     const lesson1 = {
         materia: 'Matemática',
         numeroEstudantes: 20,
         professor: 'Maria Clara',
         turno: 'manhã',
       };
-    expect(getValueByNumber(lesson1, 'materia')).toBe('Matemática');
-    expect(getValueByNumber(lesson1, 'turno')).toBe('manhã');
+    expect(getValueByNumber(lesson1, '0')).toBe('Matemática');
+    expect(getValueByNumber(lesson1, '3')).toBe('manhã');
 })
