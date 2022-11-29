3cb3efe4a7a613d651be321d35deab92c1561162
devpedroval <prmval@gmail.com>
2022-11-29 15:23:30 -0300

    parafixar 3

---
+++ Fundamentos/High-Order-Funcions-JS/dia01/parafixar3.js
@@ -0,0 +1,16 @@
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => arr.some((nome) => nome === name);
+
+const people = [
+    { name: 'Mateus', age: 20 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 21 },
+  ];
+  
+const verifyAges = (arr, idade) => arr.every((num) => num.age > idade);
+  
+  console.log(verifyAges(people, 18));
+  console.log(verifyAges(people, 14));
