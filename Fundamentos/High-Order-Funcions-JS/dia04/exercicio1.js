c1ea9ce491dbef19201ed73134e92b7c24a26b5f
devpedroval <prmval@gmail.com>
2022-12-06 15:30:52 -0300

    exercicio 3

--- Fundamentos/High-Order-Funcions-JS/dia04/exercicio1.js
+++ Fundamentos/High-Order-Funcions-JS/dia04/exercicio1.js
@@ -16,4 +16,33 @@ const sum = (...n) => {
     }
     return total;
 }
-console.log(sum(4,5,6,5))
+
+// name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade
+
+const alex = {
+    name: 'Alex',
+    age: 26,
+    likes: ['fly fishing'],
+    nationality: 'Australian',
+  };
+  
+  const gunnar = {
+    name: 'Gunnar',
+    age: 30,
+    likes: ['hiking', 'scuba diving', 'taking pictures'],
+    nationality: 'Icelandic',
+  };
+  
+
+
+
+  // complete a assinatura da função abaixo
+  const personLikes = (parameter) => {
+     const { name, age, likes, nationality} = parameter;
+    return `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+}
+  // <nome> tem <anos de idade> e gosta de <gostos da pessoa>
+  
+  // Retornos esperados:
+  console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+  console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
