b466bd6914e22ad72c9e49dc2dce6fe53f3a7dca
devpedroval <prmval@gmail.com>
2023-01-10 15:07:26 -0300

    finalizando aplicação

---
+++ Front End/Introdução/dia01/exercicio_aula/src/main.js
@@ -0,0 +1,9 @@
+import { nanoid } from "nanoid";
+
+const button = document.getElementById('botao');
+
+const text = document.getElementById('text');
+
+button.addEventListener('click', () => {
+    text.innerText = nanoid();
+});
