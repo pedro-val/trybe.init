095e7b56b5026136a301f743dd8044bb08f70d5c
devpedroval <prmval@gmail.com>
2023-01-10 18:46:37 -0300

    avançando no exercicio

--- Front End/Introdução/dia01/exerciciododia/src/main.js
+++ Front End/Introdução/dia01/exerciciododia/src/main.js
@@ -1,15 +1,38 @@
-import validador from 'validator';
+import validator from 'validator';
 
 const botao = document.getElementById('button');
 const inputArea = document.getElementById('valor');
 const select = document.getElementById('select');
-
-botao.addEventListener('click', (event) => {
-    console.log(captureValue());
-    event.preventDefault();
-})
+const result = document.getElementById('resultado');
 
 const captureValue = () => {
-    const selectedArea = select.options[select.selectedIndex]
+    const selectedArea = select.options[select.selectedIndex];
     return selectedArea.text;
-}
+};
+
+const validEmail = () => {
+    if (validator.isEmail(inputArea.value)) {
+        result.innerHTML = 'True!';
+    } else {
+        result.innerHTML = 'False!';
+    }
+};
+
+const validMoeda = () => {
+    if(validator.isCurrency(inputArea.value)) {
+        result.innerHTML = 'True!';
+    }
+};
+
+botao.addEventListener('click', (event) => {
+    const value = captureValue();
+    if (value === 'Email') {
+        validEmail();        
+    }
+    if (value === 'Valor em moeda') {
+        validMoeda();        
+    }
+    event.preventDefault();
+    
+});
+
