ea0d178efee3e5bebba869ade7186b01b0bf6ed8
devpedroval <prmval@gmail.com>
2023-01-11 16:08:21 -0300

    primeiros passos promisse

---
+++ Front End/Introdução/dia02/conteudocourse/promisse.js
@@ -0,0 +1,19 @@
+const promisses = () => {
+    
+    return new Promise((resolve, rejected) => {
+        setTimeout(() => {
+            const number = Math.round(Math.random() * 50);
+            if (number % 2 === 0) {
+                resolve(number);
+            }
+            rejected(new Error(`Número gerado é ímpar, não valido. ${number}`));
+        }, 1000)
+    })
+}
+
+promisses().then((response) => {
+    console.log(response)
+})
+.catch((error) => {
+    console.log(error.message)
+})
