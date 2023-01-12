0f147b0cfbaa7ca573d0e1fa565b8c6a5d5cf8d1
devpedroval <prmval@gmail.com>
2023-01-12 08:56:32 -0300

    tentando melhorar o codigo

--- Front End/Introdução/dia02/superHeroes/main.js
+++ Front End/Introdução/dia02/superHeroes/main.js
@@ -19,7 +19,8 @@ const getRandonHero = (event) => {
         }))
     }
     errorMessage('Erro! Tente outra vez!')
-    imagem.src = ''; 
+    imagem.src = 'http://www.blogdogasparetto.com.br/wp-content/uploads/2013/04/falha_gestao_2.jpg';
+    event.preventDefault();    
 }
 
 botao.addEventListener('click', () => getRandonHero());
