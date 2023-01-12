7714093eba9f6e2b91b8a64d45d9934eb3974011
devpedroval <prmval@gmail.com>
2023-01-12 08:52:41 -0300

    superheros com erros, acho eu

--- Front End/Introdução/dia02/superHeroes/main.js
+++ Front End/Introdução/dia02/superHeroes/main.js
@@ -4,14 +4,22 @@ const tagName = document.getElementById('nameHero');
 const botao = document.getElementById('button');
 const imagem = document.getElementById('imagem');
 
-const getRandonHero = () => {
-    const idNumber = Math.round(Math.random() * 500);
+function errorMessage(errorMessage) {
+  tagName.innerText = errorMessage;
+}
+
+const getRandonHero = (event) => {
+    const idNumber = Math.round(Math.random() * 750);
+    if (idNumber <= 500){
     fetch(`https://www.superheroapi.com/api.php/6767071873319305/${idNumber}`)
       .then((data) => data.json()
         .then((info) => {
             tagName.innerText = info.name;
             imagem.src = info.image.url;
         }))
+    }
+    errorMessage('Erro! Tente outra vez!')
+    imagem.src = ''; 
 }
 
 botao.addEventListener('click', () => getRandonHero());
